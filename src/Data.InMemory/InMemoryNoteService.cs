fdf3bc7729c48bfbaac08a18cce6344e0fd8ba5a
kwokkan <kwokkan@users.noreply.github.com>
2020-06-15 21:17:19 +0100

    Initial data.

--- src/Data.InMemory/InMemoryNoteService.cs
+++ src/Data.InMemory/InMemoryNoteService.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 using NoteDotNet.Data.Abstractions;
 using NoteDotNet.Models;
@@ -7,19 +10,88 @@ namespace NoteDotNet.Data.InMemory
 {
     public class InMemoryNoteService : INoteService
     {
+        private static IDictionary<int, NoteModel> _notes;
+        private static int _nextId;
+
+        public InMemoryNoteService()
+        {
+            SeedData();
+        }
+
         Task<int> INoteService.CreateAsync(NoteModel note)
         {
-            throw new System.NotImplementedException();
+            var now = DateTime.UtcNow;
+            var newNote = new NoteModel
+            {
+                Id = _nextId++,
+                Created = now,
+                Updated = now,
+                Content = note.Content,
+                Tags = note.Tags
+            };
+
+            _notes.Add(newNote.Id, newNote);
+
+            return Task.FromResult(newNote.Id);
         }
 
         Task<NoteModel> INoteService.GetAsync(int id)
         {
-            throw new System.NotImplementedException();
+            if (_notes.TryGetValue(id, out var note))
+            {
+                return Task.FromResult(note);
+            }
+
+            throw new Exception("Note not found");
         }
 
         Task<NoteModel[]> INoteService.SearchAsync()
         {
-            throw new System.NotImplementedException();
+            var notes = _notes.Values.ToArray();
+
+            return Task.FromResult(notes);
+        }
+
+        private static void SeedData()
+        {
+            _notes = new Dictionary<int, NoteModel>();
+
+            var now = DateTime.UtcNow;
+            _notes.Add(1, new NoteModel
+            {
+                Id = 1,
+                Created = now,
+                Updated = now,
+                Content = "# Hello World"
+            });
+
+            _notes.Add(2, new NoteModel
+            {
+                Id = 2,
+                Created = now,
+                Updated = now,
+                Content = "# With tags",
+                Tags = new string[]
+                {
+                    "first tag",
+                    "test"
+                }
+            });
+
+            _notes.Add(3, new NoteModel
+            {
+                Id = 3,
+                Created = now,
+                Updated = now,
+                Content = "# More tags",
+                Tags = new string[]
+                {
+                    "test",
+                    "more tag"
+                }
+            });
+
+            _nextId = _notes.Count + 1;
         }
     }
 }
