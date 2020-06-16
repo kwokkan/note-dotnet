b223e62d97c16f2dc11ced8f7942685eff53702d
kwokkan <kwokkan@users.noreply.github.com>
2020-06-16 20:40:33 +0100

    Fetch list of notes.

---
+++ src/Web/Components/IndexComponentBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+
+using NoteDotNet.Data.Abstractions;
+using NoteDotNet.Models;
+
+namespace NoteDotNet.Web
+{
+    public class IndexComponentBase : ComponentBase
+    {
+        [Inject]
+        private INoteService _noteService { get; set; }
+
+        protected NoteModel[] Notes { get; private set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            Notes = await _noteService.SearchAsync();
+        }
+    }
+}
