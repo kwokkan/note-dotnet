a947125fa664467923043378fa96224e9b7d552a
kwokkan <kwokkan@users.noreply.github.com>
2020-07-12 20:08:52 +0100

    Move note into own component.

---
+++ src/Web/Shared/Note.blazor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using NoteDotNet.Abstractions;
+
+namespace NoteDotNet.Web.Shared
+{
+    public class NoteComponentBase : ComponentBase
+    {
+        [Parameter]
+        public NoteModel Note { get; set; }
+    }
+}
